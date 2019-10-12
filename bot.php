d25a933631d029b35356388b5345659a24cca061
Ondřej Niesner <ondra.niesner@seznam.cz>
2019-10-12 11:33:16 +0200

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,13 +3,9 @@ $cf = require __DIR__ . "/config.php";
 require_once __DIR__ . "/{$cf["framework"]}";
 date_default_timezone_set($cf["deleter"]["timezone"]);
 
-/**
- * TeamSpeak3 Channel Deleter
- */
-
 TeamSpeak3::init();
 
-msg("Bot Started | ChannelDeleter 1.4");
+msg("Bot Started | ChannelDeleter 1.5");
 msg("PHP " . phpversion() . " | TS3Lib " . TeamSpeak3::LIB_VERSION);
 msg("Bot by Ondra3211 | https://github.com/Ondra3211" . PHP_EOL);
 $lastUpdate = time();
@@ -20,60 +16,35 @@ if (!$cf["deleter"]["warning_time"]["enabled"]) {
     $cf["deleter"]["warning_time"]["info_channel"]["description"]["enabled"] = false;
 }
 
-try
-{
+try {
     TeamSpeak3_Helper_Signal::getInstance()->subscribe("notifyServerselected", "onSelect");
 
     $uri = "serverquery://" . rawurlencode($cf["connect"]["username"]) . ":" . rawurlencode($cf["connect"]["password"]) . "@{$cf["connect"]["host"]}:{$cf["connect"]["qport"]}/?server_port={$cf["connect"]["vport"]}&nickname=" . rawurlencode($cf["connect"]["nickname"] . "&blocking=0&timeout=5");
     $ts3 = TeamSpeak3::factory($uri);
-    Deleter();
-    while (1) {
-        $ts3->getAdapter()->wait();
-    }
+    while (1) $ts3->getAdapter()->wait();
 
 } catch (TeamSpeak3_Exception $e) {
     msg("Error " . $e->getCode() . ": " . $e->getMessage());
 }
 
-function msg($message)
-{
-    echo "[" . date("d.m.Y H:i:s") . "] {$message}" . PHP_EOL;
-}
-
-function onTimeout($seconds, TeamSpeak3_Adapter_ServerQuery $adapter)
-{
-    global $cf, $lastUpdate, $ts3;
-    if ($adapter->getQueryLastTimestamp() < time() - 250) {
-        $adapter->request("clientupdate");
-    }
-    if (time() - $lastUpdate >= $cf["deleter"]["update_interval"]) {
-        Deleter();
-        $lastUpdate = time();
-    }
-}
-
-function onSelect(TeamSpeak3_Node_Host $host)
-{
+function onSelect(TeamSpeak3_Node_Host $host) {
     global $cf;
     TeamSpeak3_Helper_Signal::getInstance()->subscribe("serverqueryWaitTimeout", "onTimeout");
-    $host->serverGetSelected()->notifyRegister("server");
     if ($cf["connect"]["default_channel"] != false) {
         $host->serverGetSelected()->clientMove($host->serverGetSelected()->whoamiGet("client_id"), $cf["connect"]["default_channel"]);
     }
     msg("Connected to: " . $host->serverGetSelected()->getProperty("virtualserver_name") . PHP_EOL);
 }
 
-function Deleter()
-{
+function Deleter() {
     global $ts3, $cf;
     $ts3->channelListReset();
     $parent = $ts3->channelGetById($cf["deleter"]["parent_channel"]);
     $channels = $parent->subChannelList();
     $desc = "";
     foreach ($channels as $channel) {
-        /**
-         * Time in channel topic
-         */
+
+        //Time in channel topic
         if ($channel["seconds_empty"] === -1) {
             if (!empty($channel["channel_topic"])) {
                 $channel["channel_topic"] = "";
@@ -89,17 +60,13 @@ function Deleter()
             }
         }
 
-        /**
-         * Channel topic time
-         */
+        //Channel topic time
         if (!empty($channel["channel_topic"])) {
             $time = strtotime($channel["channel_topic"]);
             if ($time) {
                 $time = time() - $time;
 
-                /**
-                 * Warning time
-                 */
+                //Warning time
                 if ($cf["deleter"]["warning_time"]["enabled"]) {
                     if ($time >= $cf["deleter"]["warning_time"]["time"]) {
                         $deltime = $time - $cf["deleter"]["delete_time"];
@@ -107,14 +74,10 @@ function Deleter()
                         $hours = floor(abs($deltime) / 3600);
                         $seconds = floor(abs($deltime) % 60);
 
-                        /**
-                         * Prepare description
-                         */
+                        //Prepare description
                         $desc .= str_replace(["[CHANNEL]", "[HOURS]", "[MINUTES]", "[SECONDS]", "[COUNT]"], ["[URL=channelid://" . $channel["cid"] . "]" . str_replace($cf["deleter"]["warning_time"]["suffix"]["suffix"], "", $channel["channel_name"]) . "[/URL]", $hours, $minutes, $seconds, count($parent->subChannelList())], $cf["deleter"]["warning_time"]["info_channel"]["description"]["description"]);
 
-                        /**
-                         * Warning suffix
-                         */
+                        //Warning suffix
                         if ($cf["deleter"]["warning_time"]["suffix"]["enabled"]) {
                             if (!strpos($channel["channel_name"], $cf["deleter"]["warning_time"]["suffix"]["suffix"]) !== false) {
                                 if (mb_strlen($channel["channel_name"]) >= 38) {
@@ -127,9 +90,7 @@ function Deleter()
                     }
                 }
 
-                /**
-                 * Delete time
-                 */
+                //Delete time
                 if ($time >= $cf["deleter"]["delete_time"]) {
                     $channel->delete(true);
                 }
@@ -138,9 +99,7 @@ function Deleter()
     }
     $ts3->channelListReset();
 
-    /**
-     * Info channel, set channel name and descripiton
-     */
+    //Info channel, set channel name and descripiton
     if ($cf["deleter"]["warning_time"]["info_channel"]["enabled"]) {
         $parent_name = $ts3->channelGetById($cf["deleter"]["warning_time"]["info_channel"]["cid"]);
         if ($cf["deleter"]["warning_time"]["info_channel"]["channel_name"]["enabled"]) {
@@ -159,3 +118,19 @@ function Deleter()
         }
     }
 }
+
+function msg($message) {
+    echo "[" . date("d.m.Y H:i:s") . "] {$message}" . PHP_EOL;
+}
+
+function onTimeout($seconds, TeamSpeak3_Adapter_ServerQuery $adapter) {
+    global $cf, $lastUpdate, $ts3;
+    if ($adapter->getQueryLastTimestamp() < time() - 250) {
+        $adapter->request("clientupdate");
+    }
+    if (time() - $lastUpdate >= $cf["deleter"]["update_interval"]) {
+        Deleter();
+        $lastUpdate = time();
+    }
+}
+?>
