8c2e8d7f65dd3c6e04720f9b0a06dfcd7826344b
Ondřej Niesner <ondra.niesner@seznam.cz>
2019-11-16 14:29:08 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# TeamSpeak Query  bot - Channel Deleter [![GitHub release](https://img.shields.io/github/v/release/Ondra3211/ts-channel-deleter?include_prereleases)](https://github.com/Ondra3211/ts-channel-deleter/releases/tag/1.5) [![GitHub license](https://img.shields.io/github/license/Ondra3211/ts-channel-deleter)](https://github.com/Ondra3211/ts-channel-deleter/blob/master/LICENSE)
+# TeamSpeak Query  bot - Channel Deleter [![GitHub release](https://img.shields.io/github/v/release/Ondra3211/ts-channel-deleter?include_prereleases)](https://github.com/Ondra3211/ts-channel-deleter/releases/tag/1.6) [![GitHub license](https://img.shields.io/github/license/Ondra3211/ts-channel-deleter)](https://github.com/Ondra3211/ts-channel-deleter/blob/master/LICENSE)
 
 ## What is this?
 
 TeamSpeak query bot that deletes unused channels after specific time.  
 Delete time is saved in channel topic so the deletion time will not be reseted after server restart.  
+Channel icons if channel is too close to delete.
 
 ## Features
 
@@ -31,48 +32,45 @@ composer require planetteamspeak/ts3-php-framework
   
   ```php
 <?php
-/* EXAMPLE CONFIGURATION FILE */
-<?php
 $cf["framework"] = "libraries/TeamSpeak3/TeamSpeak3.php";
-$cf["connect"] = [
-	"username" => "serveradmin",
-	"password" => "2lM3Nop6",
-	"host" => "127.0.0.1",
-	"qport" => "10011",
-	"vport" => "9987",
-	"nickname" => "Channel Deleter",
-	"default_channel" => false //channel id of channel / false to disable
+$cf["connect"]   = [
+    "username"        => "serveradmin",
+    "password"        => "2lM3Nop6",
+    "host"            => "127.0.0.1",
+    "qport"           => "10011",
+    "vport"           => "9987",
+    "nickname"        => "Channel Deleter",
+    "default_channel" => false, //channel id of channel / false to disable
 ];
 $cf["deleter"] = [
-	"update_interval" => 120, //Check channels every x seconds
-	"timezone" => "Europe/Prague", //timezones https://www.php.net/manual/en/timezones.php
-	"parent_channel" => 685, //parent channel of sub channels
-	"delete_time" => 604800, //In seconds! (1 week)
-	"warning_time" => [
-		"enabled" => true, //false to disable
-		"time" => 432000, //In seconds! (2 days before delete)
-		"suffix" => [
-			"enabled" => true,
-			"suffix" => " ❗" // IMPORTANT!!! add space before the suffix 
-		],
-		"info_channel" => [ //avaiable variables [COUNT], [HOURS], [MINUTES], [SECONDS]
-			"cid" => 685, //channel id
-			"channel_name" => [
-				"enabled" => true, //false to disable
-				"channel_name" => "[cspacer]Number of rooms [COUNT]/50"
-			],
-			"description" => [ // IMPORTANT!!! Only if warning time is enabled!
-				"enabled" => true, //false to disable //if warning time is disabled this is disabled also
-				"description_prefix" => "[SIZE=10]list of rooms that will be deleted[/SIZE]", // "\n" for new line
-				"description_suffix" => "", // "\n" for new line
-				"description" => "Channel [B][CHANNEL][/B] will be deleted in [HOURS] hours, [MINUTES] minutes and [SECONDS] seconds\n", // "\n" for new line
-				"description_empty" => "No channel will be deleted" // "\n" for new line
-			]
-		]
-	]
+    "update_interval" => 120, //Check channels every x seconds
+    "timezone"        => "Europe/Prague", //timezones https://www.php.net/manual/en/timezones.php
+    "parent_channel"  => 685, //parent channel of sub channels
+    "delete_time"     => 604800, //In seconds! (1 week)
+    "warning_time"    => [
+        "enabled"      => true, //false to disable
+        "time"         => 432000, //In seconds! (2 days before delete)
+        "suffix"       => [
+            "enabled" => true,
+            "suffix"  => 4145625407, // IMPORTANT!!! add space before the suffix
+        ],
+        "info_channel" => [ //avaiable variables [COUNT], [HOURS], [MINUTES], [SECONDS]
+            "enabled"      => true, //eneble info channel, false to disable all feauters
+            "cid"          => 685, //channel id
+            "channel_name" => [
+                "enabled"      => true, //false to disable
+                "channel_name" => "[cspacer]Number of rooms [COUNT]/50",
+            ],
+            "description"  => [ // IMPORTANT!!! Only if warning time is enabled!
+                "enabled"            => true, //false to disable //if warning time is disabled this is disabled also
+                "description_prefix" => "[SIZE=10]list of rooms that will be deleted[/SIZE]", // "\n" for new line
+                "description_suffix" => "", // "\n" for new line
+                "description"        => "Channel [B][CHANNEL][/B] will be deleted in [HOURS] hours, [MINUTES] minutes and [SECONDS] seconds\n", // "\n" for new line
+                "description_empty"  => "No channel will be deleted", // "\n" for new line
+            ],
+        ],
+    ],
 ];
-return $cf;
-?>
   ```
   
 </details>
--- bot.php
+++ bot.php
@@ -1,136 +1,131 @@
 <?php
-$cf = require __DIR__ . "/config.php";
-require_once __DIR__ . "/{$cf["framework"]}";
-date_default_timezone_set($cf["deleter"]["timezone"]);
+define('VERSION', 1.6);
+
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/' . $cf['framework'];
+
+if (!$cf['deleter']['warning_time']['enabled']) {
+    $cf['deleter']['warning_time']['suffix']['enabled']                       = false;
+    $cf['deleter']['warning_time']['info_channel']['channel_name']['enabled'] = false;
+    $cf['deleter']['warning_time']['info_channel']['description']['enabled']  = false;
+}
+
+date_default_timezone_set($cf['deleter']['timezone']);
 
 TeamSpeak3::init();
 
-msg("Bot Started | ChannelDeleter 1.5");
-msg("PHP " . phpversion() . " | TS3Lib " . TeamSpeak3::LIB_VERSION);
-msg("Bot by Ondra3211 | https://github.com/Ondra3211" . PHP_EOL);
-$lastUpdate = time();
-
-if (!$cf["deleter"]["warning_time"]["enabled"]) {
-    $cf["deleter"]["warning_time"]["suffix"]["enabled"] = false;
-    $cf["deleter"]["warning_time"]["info_channel"]["channel_name"]["enabled"] = false;
-    $cf["deleter"]["warning_time"]["info_channel"]["description"]["enabled"] = false;
-}
+msg('Bot Started | ChannelDeleter ' . VERSION);
+msg('PHP ' . phpversion() . ' | TS3Lib ' . TeamSpeak3::LIB_VERSION);
+msg('Bot by Ondra3211 | https://github.com/Ondra3211' . PHP_EOL);
 
 try {
-    TeamSpeak3_Helper_Signal::getInstance()->subscribe("notifyServerselected", "onSelect");
-
-    $uri = "serverquery://" . rawurlencode($cf["connect"]["username"]) . ":" . rawurlencode($cf["connect"]["password"]) . "@{$cf["connect"]["host"]}:{$cf["connect"]["qport"]}/?server_port={$cf["connect"]["vport"]}&nickname=" . rawurlencode($cf["connect"]["nickname"] . "&blocking=0&timeout=5");
+    $uri = 'serverquery://' . rawurlencode($cf['connect']['username']) . ':' . rawurlencode($cf['connect']['password']) . "@{$cf['connect']['host']}:{$cf['connect']['qport']}/?server_port={$cf['connect']['vport']}&nickname=" . rawurlencode($cf['connect']['nickname'] . '&blocking=0&timeout=3');
     $ts3 = TeamSpeak3::factory($uri);
-    while (1) $ts3->getAdapter()->wait();
+    
+    msg('Connected to: ' . $ts3->getProperty('virtualserver_name') . PHP_EOL);
+
+    if ($cf["connect"]["default_channel"] != false) {
+        $ts3->clientMove($ts3->whoamiGet('client_id'), $cf['connect']['default_channel']);
+    }
+
+    while (1) deleter($cf['deleter']['update_interval']);
 
 } catch (TeamSpeak3_Exception $e) {
-    msg("Error " . $e->getCode() . ": " . $e->getMessage());
+    msg('[ERROR] ' . $e->getCode() . ': ' . $e->getMessage());
 }
 
-function onSelect(TeamSpeak3_Node_Host $host) {
-    global $cf;
-    TeamSpeak3_Helper_Signal::getInstance()->subscribe("serverqueryWaitTimeout", "onTimeout");
-    if ($cf["connect"]["default_channel"] != false) {
-        $host->serverGetSelected()->clientMove($host->serverGetSelected()->whoamiGet("client_id"), $cf["connect"]["default_channel"]);
-    }
-    msg("Connected to: " . $host->serverGetSelected()->getProperty("virtualserver_name") . PHP_EOL);
-}
-
-function Deleter() {
+function deleter($sleep)
+{
     global $ts3, $cf;
+
     $ts3->channelListReset();
-    $parent = $ts3->channelGetById($cf["deleter"]["parent_channel"]);
+    $parent   = $ts3->channelGetById($cf['deleter']['parent_channel']);
     $channels = $parent->subChannelList();
-    $desc = "";
+    $desc     = '';
+
     foreach ($channels as $channel) {
 
         //Time in channel topic
-        if ($channel["seconds_empty"] === -1) {
-            if (!empty($channel["channel_topic"])) {
-                $channel["channel_topic"] = "";
-                if ($cf["deleter"]["warning_time"]["suffix"]["enabled"]) {
-                    if (!strpos($channel["channel_name"], $cf["deleter"]["warning_time"]["suffix"]["suffix"]) === false) {
-                        $channel["channel_name"] = str_replace($cf["deleter"]["warning_time"]["suffix"]["suffix"], "", $channel["channel_name"]);
-                    }
-                }
+        if ($channel['seconds_empty'] === -1) {
+            if (!empty($channel['channel_topic'])) {
+                $channel['channel_topic'] = '';
             }
+            delicon($channel);
         } else {
-            if (empty($channel["channel_topic"])) {
-                $channel["channel_topic"] = date("d.m.Y H:i:s");
+            if (empty($channel['channel_topic'])) {
+                $channel['channel_topic'] = date('d.m.Y H:i:s');
             }
         }
 
         //Channel topic time
-        if (!empty($channel["channel_topic"])) {
-            $time = strtotime($channel["channel_topic"]);
+        if (!empty($channel['channel_topic'])) {
+            $time = strtotime($channel['channel_topic']);
             if ($time) {
                 $time = time() - $time;
 
-                //Warning time
-                if ($cf["deleter"]["warning_time"]["enabled"]) {
-                    if ($time >= $cf["deleter"]["warning_time"]["time"]) {
-                        $deltime = $time - $cf["deleter"]["delete_time"];
+                if ($time < $cf['deleter']['delete_time'] && $time >= $cf['deleter']['warning_time']['time']) {
+                    if ($cf['deleter']['warning_time']['enabled']) {
+                        $deltime = $time - $cf['deleter']['delete_time'];
                         $minutes = floor(abs($deltime) / 60 % 60);
-                        $hours = floor(abs($deltime) / 3600);
+                        $hours   = floor(abs($deltime) / 3600);
                         $seconds = floor(abs($deltime) % 60);
 
                         //Prepare description
-                        $desc .= str_replace(["[CHANNEL]", "[HOURS]", "[MINUTES]", "[SECONDS]", "[COUNT]"], ["[URL=channelid://" . $channel["cid"] . "]" . str_replace($cf["deleter"]["warning_time"]["suffix"]["suffix"], "", $channel["channel_name"]) . "[/URL]", $hours, $minutes, $seconds, count($parent->subChannelList())], $cf["deleter"]["warning_time"]["info_channel"]["description"]["description"]);
+                        $desc .= str_replace(['[CHANNEL]', '[HOURS]', '[MINUTES]', '[SECONDS]', '[COUNT]'], ['[URL=channelid://' . $channel['cid'] . ']' . $channel['channel_name'] . '[/URL]', $hours, $minutes, $seconds, count($parent->subChannelList())], $cf['deleter']['warning_time']['info_channel']['description']['description']);
 
                         //Warning suffix
-                        if ($cf["deleter"]["warning_time"]["suffix"]["enabled"]) {
-                            if (!strpos($channel["channel_name"], $cf["deleter"]["warning_time"]["suffix"]["suffix"]) !== false) {
-                                if (mb_strlen($channel["channel_name"]) >= 38) {
-                                    $channel["channel_name"] = substr($channel["channel_name"], 0, -3) . $cf["deleter"]["warning_time"]["suffix"]["suffix"];
-                                } else {
-                                    $channel["channel_name"] = $channel["channel_name"] . $cf["deleter"]["warning_time"]["suffix"]["suffix"];
-                                }
+                        if ($cf['deleter']['warning_time']['suffix']['enabled']) {
+                            if (!array_key_exists(143, $channel->permList()) || $channel->permList()[143]['permvalue'] === 0) {
+                                $channel->permAssign('i_icon_id', $cf['deleter']['warning_time']['suffix']['suffix']);
                             }
                         }
                     }
-                }
-
-                //Delete time
-                if ($time >= $cf["deleter"]["delete_time"]) {
+                } elseif ($time < $cf['deleter']['warning_time']['time']) {
+                    delicon($channel);
+                } else {
                     $channel->delete(true);
+                    $ts3->channelListReset();
                 }
+            } else {
+                msg('[ERROR] Failed to read time in "' . $channel['channel_name'] . '" ChannelID: ' . $channel['cid']);
             }
         }
     }
-    $ts3->channelListReset();
 
     //Info channel, set channel name and descripiton
-    if ($cf["deleter"]["warning_time"]["info_channel"]["enabled"]) {
-        $parent_name = $ts3->channelGetById($cf["deleter"]["warning_time"]["info_channel"]["cid"]);
-        if ($cf["deleter"]["warning_time"]["info_channel"]["channel_name"]["enabled"]) {
-            if ($parent_name["channel_name"] != str_replace("[COUNT]", count($parent->subChannelList()), $cf["deleter"]["warning_time"]["info_channel"]["channel_name"]["channel_name"])) {
-                $parent_name["channel_name"] = str_replace("[COUNT]", count($parent->subChannelList()), $cf["deleter"]["warning_time"]["info_channel"]["channel_name"]["channel_name"]);
+    if ($cf['deleter']['warning_time']['info_channel']['enabled']) {
+        $parent_name = $ts3->channelGetById($cf['deleter']['warning_time']['info_channel']['cid']);
+        if ($cf['deleter']['warning_time']['info_channel']['channel_name']['enabled']) {
+            if ($parent_name['channel_name'] != str_replace('[COUNT]', count($parent->subChannelList()), $cf['deleter']['warning_time']['info_channel']['channel_name']['channel_name'])) {
+                $parent_name['channel_name'] = str_replace('[COUNT]', count($parent->subChannelList()), $cf['deleter']['warning_time']['info_channel']['channel_name']['channel_name']);
             }
         }
 
-        if ($cf["deleter"]["warning_time"]["info_channel"]["description"]["enabled"]) {
+        if ($cf['deleter']['warning_time']['info_channel']['description']['enabled']) {
             if (empty($desc)) {
-                $desc = $cf["deleter"]["warning_time"]["info_channel"]["description"]["description_empty"] . PHP_EOL;
+                $desc = $cf['deleter']['warning_time']['info_channel']['description']['description_empty'] . PHP_EOL;
             }
-            if ($parent_name["channel_description"] != $cf["deleter"]["warning_time"]["info_channel"]["description"]["description_prefix"] . PHP_EOL . $desc . $cf["deleter"]["warning_time"]["info_channel"]["description"]["description_suffix"]) {
-                $parent_name["channel_description"] = $cf["deleter"]["warning_time"]["info_channel"]["description"]["description_prefix"] . PHP_EOL . $desc . $cf["deleter"]["warning_time"]["info_channel"]["description"]["description_suffix"];
+            if ($parent_name['channel_description'] != $cf['deleter']['warning_time']['info_channel']['description']['description_prefix'] . PHP_EOL . $desc . $cf['deleter']['warning_time']['info_channel']['description']['description_suffix']) {
+                $parent_name['channel_description'] = $cf['deleter']['warning_time']['info_channel']['description']['description_prefix'] . PHP_EOL . $desc . $cf['deleter']['warning_time']['info_channel']['description']['description_suffix'];
             }
+        }
+    }
+    sleep($sleep);
+}
+
+function delicon($channel) {
+    global $cf;
+
+    if ($cf['deleter']['warning_time']['suffix']['enabled']) {
+        if (array_key_exists(143, $channel->permList())) {
+            $channel->permRemove('i_icon_id');
         }
     }
 }
 
-function msg($message) {
-    echo "[" . date("d.m.Y H:i:s") . "] {$message}" . PHP_EOL;
+function msg($message)
+{
+    echo '[' . date('d.m.Y H:i:s') . "] {$message}" . PHP_EOL;
 }
 
-function onTimeout($seconds, TeamSpeak3_Adapter_ServerQuery $adapter) {
-    global $cf, $lastUpdate, $ts3;
-    if ($adapter->getQueryLastTimestamp() < time() - 250) {
-        $adapter->request("clientupdate");
-    }
-    if (time() - $lastUpdate >= $cf["deleter"]["update_interval"]) {
-        Deleter();
-        $lastUpdate = time();
-    }
-}
 ?>
